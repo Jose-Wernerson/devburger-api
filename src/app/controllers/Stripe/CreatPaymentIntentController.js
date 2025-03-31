76b5b35bd86e7335e7f3f0dd25812bc9b3101d41
José-Wernerson <wemersonranter@gmail.com>
2025-03-30 21:16:04 -0300

    key stripe

--- src/app/controllers/Stripe/CreatPaymentIntentController.js
+++ src/app/controllers/Stripe/CreatPaymentIntentController.js
@@ -2,53 +2,53 @@ import Stripe from 'stripe'
 import * as Yup from 'yup'
 import 'dotenv/config'
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
+const stripe = require('stripe')(
+	'sk_test_51R8Sa9RehNutrk7WnT9gtoW8D1bRqeiQEIggqh7KSDZhdbF7qJnOLTZ8ICzIohNKYgLqrXAlIZRBtEvkTBy3MTts00GboxikbU',
+)
 const calculateOrderAmount = (items) => {
-    const total = items.reduce((acc, current) => {
-        return current.price * current.quantity + acc;
-    }, 0);
+	const total = items.reduce((acc, current) => {
+		return current.price * current.quantity + acc
+	}, 0)
 
-    return total;
+	return total
 }
 class CreatPaymentIntentController {
 	async store(request, response) {
-		    const schema = Yup.object({
-                    products: Yup.array()
-                        .required()
-                        .of(
-                            Yup.object({
-                                id: Yup.number().required(),
-                                quantity: Yup.number().required(),
-                                price: Yup.number().required(),
-                            }),
-                        ),
-                })
+		const schema = Yup.object({
+			products: Yup.array()
+				.required()
+				.of(
+					Yup.object({
+						id: Yup.number().required(),
+						quantity: Yup.number().required(),
+						price: Yup.number().required(),
+					}),
+				),
+		})
 
-                try {
-                    schema.validateSync(request.body, { abortEarly: false })
-                } catch (err) {
-                    return response.status(400).json({ errors: err.errors })
-                }
+		try {
+			schema.validateSync(request.body, { abortEarly: false })
+		} catch (err) {
+			return response.status(400).json({ errors: err.errors })
+		}
 
-                const { products } = request.body
+		const { products } = request.body
 
-                const amount = calculateOrderAmount(products)
+		const amount = calculateOrderAmount(products)
 
-                const paymentIntent = await stripe.paymentIntents.create({
-                    amount,
-                    currency: 'brl',
-                    automatic_payment_methods: {
-                      enabled: true,
-                    },
-                  });
-                  console.log(paymentIntent.status)
-                  response.json({
-                    clientSecret: paymentIntent.client_secret,
-                    dpmCheckerLink: `https://dashboard.stripe.com/settings/payment_methods/review?transaction_id=${paymentIntent.id}`,
-                  });
-                  
+		const paymentIntent = await stripe.paymentIntents.create({
+			amount,
+			currency: 'brl',
+			automatic_payment_methods: {
+				enabled: true,
+			},
+		})
+		console.log(paymentIntent.status)
+		response.json({
+			clientSecret: paymentIntent.client_secret,
+			dpmCheckerLink: `https://dashboard.stripe.com/settings/payment_methods/review?transaction_id=${paymentIntent.id}`,
+		})
 	}
 }
-
 
 export default new CreatPaymentIntentController()
